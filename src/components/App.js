6792110ff0f00c51bd57e763ad5efd1bdafed169
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-11-03 07:31:02 -0700

    building HomePage

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import { Home } from "../../styles/PageStyles/HomePageStyles";
+
+
+export default function HomePage(){
+
+
+    return(
+        <Home className="home">
+            home is here
+        </Home>
+    )
+}

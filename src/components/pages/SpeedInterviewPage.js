8729f7ae577bec6be92fb4c319d9203229d69cdc
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-11-03 23:29:59 -0700

    started SpeedInterviewPage

---
+++ src/components/pages/SpeedInterviewPage.js
@@ -0,0 +1,8 @@
+
+
+export default function SpeedInterviewPage(){
+
+    // here you would write a chat box styled page of 
+    // typically asked interview questions and 
+    // of what your responses would be
+}

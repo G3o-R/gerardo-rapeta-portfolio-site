e821b2f512abb6b669fb57ea9d2d6bd3ce6d53ab
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-11-03 23:27:25 -0700

    building AboutPage

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,10 +1,47 @@
-import { About } from "../../styles/PageStyles/AboutPageStyles"
+import {
+  About,
+  AboutMeContainer,
+  AboutMeWrapper,
+  EducationContainer,
+  EducationWrapper,
+  HeaderContainer,
+  HeaderWrapper,
+  LanguagesColumnContainer,
+  TechnicalSkillsContainer,
+  TechnicalSkillsWrapper,
+} from "../../styles/PageStyles/AboutPageStyles";
 
-export default function AboutPage(){
-
-    return (
-        <About>
-            test
-        </About>
-    )
+export default function AboutPage() {
+  return (
+    <About>
+      <HeaderWrapper>
+        <HeaderContainer className="About"> <h1>About</h1></HeaderContainer>
+      </HeaderWrapper>
+      <AboutMeWrapper>
+        <AboutMeContainer>
+          <p> lots and lots of text here abouit yourself</p>
+        </AboutMeContainer>
+      </AboutMeWrapper>
+      <HeaderWrapper>
+        <HeaderContainer className="Education"> <h1>Education</h1></HeaderContainer>
+      </HeaderWrapper>
+      <EducationWrapper>
+        <EducationContainer>
+            <h1>Flatiron School New York, New York</h1>
+            <p>text from resume here</p>
+        </EducationContainer>
+      </EducationWrapper>
+      <HeaderWrapper>
+        <HeaderContainer className="Skills"> <h1>Skills</h1></HeaderContainer>
+      </HeaderWrapper>
+      <TechnicalSkillsWrapper>
+        <TechnicalSkillsContainer>
+            <LanguagesColumnContainer> 
+                {/* svgs would be here like JS Ruby Python etc */}
+            </LanguagesColumnContainer>
+            {/* here you would write other skills */}
+        </TechnicalSkillsContainer>
+      </TechnicalSkillsWrapper>
+    </About>
+  );
 }
--- src/styles/PageStyles/AboutPageStyles.js
+++ src/styles/PageStyles/AboutPageStyles.js
@@ -4,3 +4,45 @@ export const About = styled.div`
 
 `;
 
+export const AboutMeWrapper = styled.div`
+
+    p{
+
+    }
+`;
+
+export const AboutMeContainer = styled.div`
+
+`;
+
+export const HeaderWrapper = styled.div`
+
+`;
+
+export const HeaderContainer = styled.div`
+
+    h1{
+
+    }
+`;
+
+export const EducationWrapper = styled.div`
+
+`;
+
+export const EducationContainer = styled.div`
+
+`;
+
+export const TechnicalSkillsWrapper = styled.div`
+
+`;
+
+export const TechnicalSkillsContainer = styled.div`
+
+`;
+
+export const LanguagesColumnContainer = styled.div`
+
+`;
+

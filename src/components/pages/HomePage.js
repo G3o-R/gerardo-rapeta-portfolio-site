6f2f3da9f874688ed2aa0b639db01d5166e43d2e
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-12-05 15:58:46 -0700

    testing something

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -8,11 +8,17 @@ import Skills from "../sections/Skills";
 
 
 export default function HomePage(){
+    const height = (window.innerHeight)
+    const width = (window.innerWidth)
 
     return (
         <StyledHomePage>
             <Header />
             <Projects />
+            <div>
+                <h1>height: {height}</h1>
+                <h1>width: {width}</h1>
+            </div>
             <About />
             <Skills />
             <Biography />

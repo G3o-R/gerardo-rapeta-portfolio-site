24db132a612b955798dde22f11f234d90c107a52
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-12-05 16:14:15 -0700

    changed all px to em

--- src/styles/PortfolioCardsStyles.js
+++ src/styles/PortfolioCardsStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MainCardContainer = styled.div`
   position: relative;
   width: 100%;
-  max-width: 400px;
+  max-width: 25em;
   aspect-ratio: 16/11;
 `;
 
@@ -13,7 +13,7 @@ export const Card = styled.div`
   height: 100%;
   transform-style: preserve-3d;
   transition: all 0.5s ease;
-  border-radius: 24px;
+  border-radius: 1.5em;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -29,16 +29,16 @@ export const FrontFace = styled.div`
   backface-visibility: hidden;
   background-color: #2d2d2d;
   background: linear-gradient(to bottom right, #1a1a1a, #2d2d2d);
-  border-radius: 24px;
+  border-radius: 1.5em; 
   overflow: hidden;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0em 0.25em 0.625em rgba(0, 0, 0, 0.3);
 `;
 
 export const ImageWrapper = styled.div`
   width: 100%;
   height: 60%;
   overflow: hidden;
-  border-bottom: 2px solid #1a1a1a;
+  border-bottom: 0.125em solid #1a1a1a;
 `;
 
 export const ImageContainer = styled.div`
@@ -63,11 +63,11 @@ export const BackFace = styled.div`
   backface-visibility: hidden;
   background: linear-gradient(to bottom right, #2d2d2d, #1a1a1a);
   transform: rotateY(180deg);
-  border-radius: 24px;
-  border: 3px solid #1a1a1a;
+  border-radius: 1.5em;
+  border: 0.1875em solid #1a1a1a;
   overflow: hidden;
   color: #dee7e1;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0em 0.25em 0.625em rgba(0, 0, 0, 0.3);
 `;
 
 export const TextContainer = styled.div`
@@ -84,17 +84,17 @@ export const TextContainer = styled.div`
   }
 
   &.back {
-    margin: 10px 10px auto;
+    margin: 0.625em 0.625em auto; 
     p {
       font-size: auto;
       line-height: 1.5;
       text-align: center;
       color: #dee7e1;
-      @media screen and (min-width: 400px) {
-        font-size: 1.125em
+      @media screen and (min-width: 25em) {
+        font-size: 1.125em;
       }
 
-      @media screen and (min-width: 568px){
+      @media screen and (min-width: 35.5em) {
         font-size: 1.25em;
       }
     }
@@ -105,7 +105,7 @@ export const LinksContainer = styled.div`
   display: flex;
   justify-content: space-around;
   width: 100%;
-  padding: 10px 0;
+  padding: 0.625em 0;
   cursor: pointer;
 
   a, div {
@@ -113,18 +113,18 @@ export const LinksContainer = styled.div`
     font-size: 1.125em;
     font-weight: bold;
     color: #ffffff;
-    padding: 10px 20px;
-    border-radius: 8px;
+    padding: 0.625em 1.25em;
+    border-radius: 0.5em;
     transition: all 0.3s ease;
     background: linear-gradient(to bottom right, #0d6759, #334534);
-    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3);
+    box-shadow: 0em 0.25em 0.375em rgba(0, 0, 0, 0.3);
 
     &:hover {
       transform: scale(1.1);
-      box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.5);
+      box-shadow: 0em 0.375em 0.625em rgba(0, 0, 0, 0.5); 
     }
 
-    &.disabled{
+    &.disabled {
       cursor: pointer;
     }
   }
